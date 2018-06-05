17e324164d00478291e804826f721c7132ce0232
YannisDEV <35256959+YannisSIO@users.noreply.github.com>
2018-06-05 13:49:40 +0200

    Maj Procedure insert MATERIEL CLIENT LOURD

--- Procedure.sql
+++ Procedure.sql
@@ -1,6 +1,7 @@
+
 drop procedure if exists PInsMat;
 delimiter //
-create procedure PInsMat (nomM char(32), prix float(10,2), ville varchar(128), codeP char(5), larue varchar (128), dateD date, dateF date, nomType char(32), etat char(32))
+create procedure PInsMat (nomM char(32), prix float(10,2), ville varchar(128), codeP char(5), larue varchar (128), imageM varchar (100), stockM int (2), nomType char(32), etat char(32))
 
 	begin
 		declare nbL, nbE, nbT, idl, ide, idt int;
@@ -28,10 +29,9 @@ create procedure PInsMat (nomM char(32), prix float(10,2), ville varchar(128), c
 
 			select id_localisation into idl from localisation where lieux = ville and cp = codeP and rue_c = larue;
 
-			insert into materiel values (null,idl,idt,ide,nomM,prix,dateD,dateF);
-
-		end if;
-
+			insert into materiel values (null,idl,idt,ide,nomM,prix,imageM,stockM);
+		
+		else
 
 		select id_etat into ide from etat where nom_etat= etat;
 
@@ -39,10 +39,14 @@ create procedure PInsMat (nomM char(32), prix float(10,2), ville varchar(128), c
 
 		select id_localisation into idl from localisation where lieux = ville and cp = codeP and rue_c = larue;
 
-		insert into materiel values (null,idl,idt,ide,nomM,prix,dateD,dateF);
+		insert into materiel values (null,idl,idt,ide,nomM,prix,imageM,stockM);
+	
+	end if;
 
 	end //
 delimiter ;
 
 
--- call PInsMat ('truc','10.2','paris','75000','rue de paris','2018-04-01',curdate(),'nettoyage','neuf');
+-- call PInsMat ('manitou','20.90','Beaumont','95260','rue de senlis','/images/manitou.jpg',10,'manutention','bon etat');
+-- call PInsMat ('grue','29.99','Chambly','60000','rue des champs','/images/grue.jpg',10,'manutention','bon etat');
+
